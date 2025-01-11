ae90c6bec4b45e0daa57b39cbacca1dbf7ee8ec5
Richard <richard1simonik@gmail.com>
2025-01-11 20:15:18 +0100

    Initial Commit

---
+++ CPRHelperv2/CPRHelperv2App.swift
@@ -0,0 +1,17 @@
+//
+//  CPRHelperv2App.swift
+//  CPRHelperv2
+//
+//  Created by Richard Šimoník on 11.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct CPRHelperv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CPRHelperv2Tests/CPRHelperv2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  CPRHelperv2Tests.swift
+//  CPRHelperv2Tests
+//
+//  Created by Richard Šimoník on 11.01.2025.
+//
+
+import Testing
+@testable import CPRHelperv2
+
+struct CPRHelperv2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
