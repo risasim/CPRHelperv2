3ec99a4dc8488c277ffb66825a59391df70f0a3b
Richard <richard1simonik@gmail.com>
2025-02-04 16:54:55 +0100

    Created first version of the onboarding.

---
+++ CPRHelperv2/Views/OnBoarding/OnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingView.swift
+//  CPRHelperv2
+//
+//  Created by Richard Šimoník on 04.02.2025.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoardingView()
+}
